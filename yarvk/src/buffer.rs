79401f05cf018213937c2da91395bacf0fbbf473
psionic12 <iamweaf@gmail.com>
2022-11-10 00:01:26 +0800

    Add function cmd_draw

--- yarvk/src/buffer.rs
+++ yarvk/src/buffer.rs
@@ -197,4 +197,23 @@ impl<const LEVEL: Level, const SCOPE: RenderPassScope, const ONE_TIME_SUBMIT: bo
             );
         }
     }
+
+    pub fn cmd_draw(
+        &mut self,
+        vertex_count: u32,
+        instance_count: u32,
+        first_vertex: u32,
+        first_instance: u32,
+    ) {
+        unsafe {
+            // Host Synchronization: commandBuffer, VkCommandPool
+            self.device.ash_device.cmd_draw(
+                self.vk_command_buffer,
+                vertex_count,
+                instance_count,
+                first_vertex,
+                first_instance,
+            );
+        }
+    }
 }
